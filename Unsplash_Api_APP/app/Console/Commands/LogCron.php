89c37d99ca7ad3f9cd051e802f22ee9e9d4f6b14
majdaleid <34122653+majdaleid@users.noreply.github.com>
2022-03-18 00:39:37 +0100

    cronjob -call all logged Users Through api automatically

--- Unsplash_Api_APP/app/Console/Commands/LogCron.php
+++ Unsplash_Api_APP/app/Console/Commands/LogCron.php
@@ -2,11 +2,23 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
+
+
+
+use App\Models\User;
 
 use App\Services\MarketService;
+
+use Illuminate\Console\Command;
+ 
 use App\Services\SaveApiRequests;
+use Illuminate\Support\Facades\Auth;
+
+use App\Traits\AuthorizesMarketRequests;
+
 use App\Services\MarketAuthenticationService;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
 
 class LogCron extends Command
 {
@@ -27,33 +39,69 @@ class LogCron extends Command
     protected $marketService;
     protected $saveApiRequests;
     protected $marketAuthenticationService;
+
+    use AuthenticatesUsers;
+    
+    protected $user;
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct(MarketAuthenticationService $marketAuthenticationService,MarketService $marketService,SaveApiRequests $saveApiRequests )
+    
+    public function __construct(MarketAuthenticationService $marketAuthenticationService,MarketService $marketService,SaveApiRequests $saveApiRequests,User $user )
     {
         $this->marketService=$marketService;
         $this->saveApiRequests=$saveApiRequests;
         $this->marketAuthenticationService = $marketAuthenticationService;
+       $this->user = $user;
         
         parent::__construct();
     }
+
+
+
 
     /**
      * Execute the console command.
      *
      * @return int
      */
+
+      //get all saved accessToken in database and call get user information 
+      //to run the cron job locally :php artisan log:cron 
     public function handle()
     {
 
-        $userData=$this->marketService->getUserInformation();
-       // $userData=$this->marketService->getPhotos();
-     //   $userInfo=$this->saveApiRequests->registerOrUpdateUserInfo($userData);
-       // return 0;
-       //  dd("huhuh");
-       //$this->info("UserInfo has been Updated successfully");
+        //if i want to get just one accessToken 
+
+       // $access_token = User::pluck('access_token');
+
+       //if i want to get the access token for the first regsitered user 
+        /*
+       $access_token= Auth::loginUsingId(1)->access_token;
+
+       $this->marketService->resolveAccessTokenUnsplash=$access_token;
+    
+       $userData=$this->marketService->getUserInformation();
+       dd($userData);
+
+      */
+
+        $accessTokenUsers = User::select('id')->get();
+     
+        foreach($accessTokenUsers  as $accessTokenUser)
+        {
+
+            $access_token= Auth::loginUsingId($accessTokenUser->id)->access_token;
+            $this->marketService->resolveAccessTokenUnsplash=$access_token;
+            $userData=$this->marketService->getUserInformation();
+            //dd($userData);
+          
+        }
+
+       $this->info("All Users that have been logged,are called through api automatically successfully");
+
+    
     }
 }
--- Unsplash_Api_APP/app/Console/Kernel.php
+++ Unsplash_Api_APP/app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use App\Console\Commands\LogCron;
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,9 +29,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('log:cron')->everyMinute();
+       $schedule->command('log:cron')->everyMinute();
 
-        
+      
     }
 
     /**
